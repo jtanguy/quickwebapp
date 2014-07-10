e19446c2a01b771206adc6b797de0125ec9ff1ca
Julien Tanguy <julien.tanguy@jhome.fr>
2014-07-10 10:16:54 +0200

    Drop the transformer stuff for now
    - Return to a minimal function taking a `BS -> BS` input
    - Create ToLBS/FromLBS classes in order to allow more general String-like type
      functions

--- QuickWebApp.hs
+++ QuickWebApp.hs
@@ -1,66 +1,38 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE FlexibleInstances         #-}
 {-# LANGUAGE OverloadedStrings         #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
--- | Create a quick-and-dirty api
---
--- > interactWeb [ reverse <?> "reverse"
--- >             , id <?> "echo"
--- >             ]
 module QuickWebApp where
 
-import           Control.Applicative
-import           Control.Monad
-import           Data.Maybe
-import           Data.Aeson              (toJSON)
 import qualified Data.ByteString.Lazy    as BL
-import qualified Data.Text.Lazy          as TL
-import qualified Data.Text.Lazy.Encoding as TL
-import           Network.Mime
+import qualified Data.ByteString  as BS
 import           Web.Scotty
 
--- | A 'Transformer' is a transformation from 'TL.Text' to 'TL.Text', with a name
--- and a 'MimeType'
-data Transformer = Transformer { name   :: String
-                               , mimeTo :: MimeType
-                               , trans  :: TL.Text -> TL.Text
-                               }
+class ToLBS a where
+    toLBS :: a -> BL.ByteString
 
--- | Convert anything into a transformer with 'HasTransformer'
-class HasTransformer m where
-    getTransformer :: m -> Transformer
+instance ToLBS BL.ByteString where
+    toLBS = id
 
--- |
-instance HasTransformer (TL.Text -> TL.Text) where
-    getTransformer = Transformer "convert" "text/plain"
+instance ToLBS BS.ByteString where
+    toLBS = BL.fromStrict
 
-instance HasTransformer (TL.Text -> Maybe TL.Text) where
-    getTransformer f = Transformer "convert" "text/plain" (fromMaybe "" . f)
+class FromLBS a where
+    fromLBS :: BL.ByteString -> a
 
--- |
-instance HasTransformer Transformer where
-    getTransformer = id
+instance FromLBS BL.ByteString where
+    fromLBS = id
 
--- | Give a transformer a name
-(<?>) :: HasTransformer m => m -> String -> Transformer
-t <?> newname = (getTransformer t) { name = newname }
-
--- | Give a transformer an output MimeType
-(<::>) :: HasTransformer m => m -> MimeType -> Transformer
-t <::> mime = (getTransformer t) { mimeTo = mime }
+instance FromLBS BS.ByteString where
+    fromLBS = BL.toStrict
 
 -- | 'interactWebOn 3000'
-interactWeb :: forall m. HasTransformer m => [m] -> IO ()
+interactWeb :: (FromLBS a, ToLBS b) => (a -> b) -> IO ()
 interactWeb = interactWebOn 3000
 
 -- | Create an API with multiple endpoints:
 --
 -- - a 'POST' endpoint per transformer (at URI 'name')
 -- - a 'GET' endpoint, which lists all the above endpoints
-interactWebOn :: forall m. HasTransformer m => Int -> [m] -> IO ()
-interactWebOn port ts = scotty port $ do
-        get "/" $ json . toJSON . map (name . getTransformer) $ ts
-        forM_ ts $ \t -> post (literal . name . getTransformer $ t) $ do
-                c <- param "contents" `rescue` const (BL.toStrict <$> body)
-                setHeader "Content-Type" (TL.decodeUtf8 . BL.fromStrict . mimeTo . getTransformer $ t)
-                raw . TL.encodeUtf8 . (trans . getTransformer $ t) . TL.decodeUtf8 . BL.fromStrict $ c
+interactWebOn :: (FromLBS a, ToLBS b) => Int -> (a -> b) -> IO ()
+interactWebOn port f = scotty port $ post "/" $ do
+                c <- body
+                setHeader "Content-Type" "text/plain"
+                raw . toLBS . f . fromLBS $ c
--- README.md
+++ README.md
@@ -1,21 +1,12 @@
 quickwebapp
 ===========
-A quick-and-dirty api generator, for any function 'Text -> Text'.
-Inspired from the 'interact' function from 'Prelude'.
+A quick-and-dirty api generator, for any function `a -> b` which can be wrapped
+inside a function `ByteString -> ByteString`.
+It is inspired from the 'interact' function from 'Prelude'.
 
 ~~~{haskell}
-interactWeb [ reverse ]
+interactWeb reverse
 ~~~
 
-This creates a server listening on port 3000. A `GET` request on the server
-returns a JSON file containing all the endpoints
-(here, we would have `[ "convert" ]`).
+This creates a server listening on port 3000.
 
-One can specify the name of the endpoint and content type with `<?>` and
-`<::>`:
-
-~~~{haskell}
-interactWeb [ reverse <?> "reverse" <::> "text/plain"
-            , id <?> "echo" <::> "text/rtf"
-            ]
-~~~
